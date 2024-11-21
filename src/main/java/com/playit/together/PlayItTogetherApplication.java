b5a3afda3303d8c31767311f45d64bc539ac41a1
wooco <96woosj@gmail.com>
2024-11-22 01:44:18 +0900

    Initial project setup

---
+++ src/main/java/com/playit/together/PlayItTogetherApplication.java
@@ -0,0 +1,13 @@
+package com.playit.together;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayItTogetherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayItTogetherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/playit/together/PlayItTogetherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.playit.together;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayItTogetherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
